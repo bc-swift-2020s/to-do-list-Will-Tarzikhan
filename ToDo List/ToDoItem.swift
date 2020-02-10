30799bd4e575623e692c4b5ab132e89b67e64a8b
Will Tarzikhan <tarzikha@bc.edu>
2020-02-10 18:16:20 -0500

    5.6 Complete

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by William Tarzikhan on 2/10/20.
+//  Copyright © 2020 William Tarzikhan. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
