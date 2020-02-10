5b324bc353684f5d168872d8e711afd0475243b3
Will Tarzikhan <tarzikha@bc.edu>
2020-02-10 15:04:03 -0500

    5.2

---
+++ ToDo List/ToDoListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by William Tarzikhan on 2/10/20.
+//  Copyright © 2020 William Tarzikhan. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection was just called. Returning \(toDoArray.count)")
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+}
